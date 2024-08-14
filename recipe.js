d4fa9bc25966fee3b6ca08fc9e5a6690246b4009
Abideen <abideenalabi1210@gmail.com>
2024-08-14 12:50:38 +0100

    First Recipe

---
+++ recipe.js
@@ -0,0 +1,12 @@
+    // const nav = document.querySelector(".nav1");
+    // const drop = document.querySelector(".drop1");
+
+    // nav.addEventListener("mouseover", () => {
+    //   drop.style.display = "block";
+    // });
+
+    // nav.addEventListener("mouseout", () => {
+    //   setTimeout(() => {
+    //     drop.style.display = "none";
+    //   }, 500);
+    // });
